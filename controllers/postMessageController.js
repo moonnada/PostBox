e68830fd05b2229fa4ef0e41f9ffc8bc7d2a72fa
Byung Moon Kim <kimbm2014@gmail.com>
2021-09-14 19:18:29 -0700

    set up the basic stuff of backend

--- controllers/postMessageController.js
+++ controllers/postMessageController.js
@@ -1,8 +1,10 @@
 const express = require('express')
 let router = express.Router()
 
+let ObjectID = require('mongoose').Types.ObjectId
 let { PostMessage } = require('../models/postMessage')
 
+//GET a DATA
 router.get('/', (req, res) => {
     PostMessage.find((err, docs) => {
         if(!err) res.send(docs)
@@ -10,5 +12,43 @@ router.get('/', (req, res) => {
     })
 })
 
+//POST a DATA
+router.post('/', (req,res) => {
+    let newRecord = new PostMessage({
+        title: req.body.title,
+        message: req.body.message
+    })
+    newRecord.save((err, docs) => {
+        if(!err) res.send(docs)
+        else console.log('Error while retrieving all records : ' + JSON.stringify(err, undefined,2))
+    })
+})
+
+//UPDATE a DATA
+router.put('/:id', (req,res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('No record with given id : ' + req.params.id)
+
+    let updatedRecord = {
+        title: req.body.title,
+        message: req.body.message
+    }
+
+    PostMessage.findByIdAndUpdate(req.params.id, {$set:updatedRecord}, (err, docs) => {
+        if(!err) res.send(docs)
+        else console.log('Error while updating all records : ' + JSON.stringify(err, undefined,2))
+    })
+})
+
+//DELETE a data
+router.delete(':id', (req,res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('No record with given id : ' + req.params.id)
+
+    PostMessage.findByIdAndDelete(req.params.id, (err, docs) => {
+        if(!err) res.send(docs)
+        else console.log('Error while deleting all records : ' + JSON.stringify(err, undefined,2))
+    })
+})
 
 module.exports = router
--- models/postMessage.js
+++ models/postMessage.js
@@ -3,6 +3,6 @@ const mongoose = require('mongoose')
 let PostMessage = mongoose.model('PostMessage', {
     title : {type: String},
     message : {type: String},
-})
+},'postMessages')
 
 module.exports = {PostMessage}
